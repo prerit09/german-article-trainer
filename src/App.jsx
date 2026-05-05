d0a436f0cb83e9ce683da91883a8c68a34f1a3e7
Prerit Jain <prerit.jain@aryza.com>
2026-05-05 16:35:47 +0200

    added ch7 vocab

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,7 @@ export default function App() {
       case "b1-ch-4": return import.meta.env.BASE_URL + "data/b1-ch-4.json";
       case "b1-ch-5": return import.meta.env.BASE_URL + "data/b1-ch-5.json";
       case "b1-ch-6": return import.meta.env.BASE_URL + "data/b1-ch-6.json";
+      case "b1-ch-7": return import.meta.env.BASE_URL + "data/b1-ch-7.json";
       default: throw new Error("Unknown level: " + level);
     }
   }
@@ -207,6 +208,7 @@ export default function App() {
               <option value="b1-ch-4">B1 - Einheit - 4</option>
               <option value="b1-ch-5">B1 - Einheit - 5</option>
               <option value="b1-ch-6">B1 - Einheit - 6</option>
+              <option value="b1-ch-7">B1 - Einheit - 7</option>
               {/* <option value="a1">A1</option>
               <option value="a2">A2</option>
               <option value="b1">B1</option> */}
